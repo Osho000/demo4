813096884c71c2b021a98d6da48e40940e3d073c
michoooooooooo <michoooooooooo@github.com>
2025-05-28 19:28:33 +0300

    new4

---
+++ src/main/java/com/example/demo4/controllers/UserController.java
@@ -0,0 +1,18 @@
+package com.example.demo4.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    @GetMapping()
+   public String getAboba() {
+        return "Aboba";
+    }
+
+}
